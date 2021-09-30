424a8dc8f34a22ff21666cacf64ec90b082f5585
khamutov <khamutov@ailabs.ru>
2021-09-30 18:08:44 +0000

    style: format files using black

--- create_splits.py
+++ create_splits.py
@@ -15,26 +15,33 @@ def clean_targets(target):
     if target.exists():
         shutil.rmtree(target)
 
+
 def hardlink(files, target_path):
     for file in files:
         target = pathlib.Path(file)
         target.link_to(target_path / target.name)
 
 
-
 def split(data_dir):
     """
-    Create three splits from the processed records. The files should be moved to new folders in the 
+    Create three splits from the processed records. The files should be moved to new folders in the
     same directory. This folder should be named train, val and test.
 
     args:
         - data_dir [str]: data directory, /mnt/data
     """
 
-    files = [filename for filename in glob.glob(os.path.join(data_dir, 'processed/*.tfrecord'))]
+    files = [
+        filename
+        for filename in glob.glob(os.path.join(data_dir, "processed/*.tfrecord"))
+    ]
 
-    train_files, val_files = sklearn.model_selection.train_test_split(files, test_size=0.3)
-    val_files, test_files = sklearn.model_selection.train_test_split(val_files, test_size=0.65)
+    train_files, val_files = sklearn.model_selection.train_test_split(
+        files, test_size=0.3
+    )
+    val_files, test_files = sklearn.model_selection.train_test_split(
+        val_files, test_size=0.65
+    )
 
     data_dir = pathlib.Path("dataset/splits")
 
@@ -43,27 +50,28 @@ def split(data_dir):
     print("test_file", len(test_files))
 
     files_mapping = {
-            "train": train_files,
-                "val": val_files,
-                    "test": test_files,
-                    }
+        "train": train_files,
+        "val": val_files,
+        "test": test_files,
+    }
 
     for dir_name, files in files_mapping.items():
         data_path = data_dir / dir_name
-            
+
         clean_targets(data_path)
         data_path.mkdir(parents=True, exist_ok=True)
-                    
+
         hardlink(files, data_path)
     # TODO: Implement function
-    
 
-if __name__ == "__main__": 
-    parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
-    parser.add_argument('--data_dir', required=True,
-                        help='data directory')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Split data into training / validation / testing"
+    )
+    parser.add_argument("--data_dir", required=True, help="data directory")
     args = parser.parse_args()
 
     logger = get_module_logger(__name__)
-    logger.info('Creating splits...')
+    logger.info("Creating splits...")
     split(args.data_dir)
